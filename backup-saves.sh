23b32e9ab9d1e93d712cf17cf4d4d11a75721d18
taylorsmcclure <taylorsmcclure@gmail.com>
2021-02-20 16:54:16 -0600

    feat: backup script now cleans up backups older than 1 day

--- backup-saves.sh
+++ backup-saves.sh
@@ -20,3 +20,7 @@ FILE_NAME="`timestamp`_backup.zip"
 echo "`timestamp` - Backing up Valheim worlds..."
 zip -r "${BACKUP_PATH}${FILE_NAME}" $SOURCE_PATH
 echo "`timestamp` - Successfully saved backup to: ${BACKUP_PATH}${FILE_NAME}"
+
+echo "`timestamp` - Deleting backups older than one day..."
+find $BACKUP_PATH -mtime +1 -type f -exec rm {} \;
+echo "`timestamp` - Deletion completed."
